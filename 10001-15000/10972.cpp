b5754861e0b80ee0120aa0480be4997214f023e4
lms0806 <godmlzkf1@naver.com>
2023-11-05 13:00:28 +0900

    Create 10972.cpp

---
+++ 10001-15000/10972.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void fastio() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void solve() {
+    int n;
+
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    if (next_permutation(v.begin(), v.end())) {
+        for (auto a : v) {
+            cout << a << " ";
+        }
+    }
+    else {
+        cout << "-1";
+    }
+}
+
+int main() {
+    fastio();
+    solve();
+}
