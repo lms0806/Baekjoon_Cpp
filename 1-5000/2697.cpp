facd53e259d739c76af1aa05f7c510e3c28ecd42
lms0806 <godmlzkf1@naver.com>
2023-11-05 16:34:02 +0900

    Create 2697.cpp

---
+++ 1-5000/2697.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void fastio() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void solve() {
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string s;
+
+        cin >> s;
+
+        vector<char> v(s.size());
+        for (int i = 0; i < s.size(); i++) {
+            v[i] = s[i];
+        }
+
+        if (next_permutation(v.begin(), v.end())) {
+            for (auto a : v) {
+                cout << a;
+            }
+        }
+        else {
+            cout << "BIGGEST";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    fastio();
+    solve();
+}
