d30096d0c34765157a341bb9d14c87b0d8a9b985
lms0806 <godmlzkf1@naver.com>
2024-01-27 15:36:19 +0900

    Create 12185.cpp

---
+++ 10001-15000/12185.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int arr[501][2];
+
+void fastio() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void solve() {
+    int size;
+    
+    cin >> size;
+
+    for(int t = 1; t <= size; t++) {
+        int n;
+
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i][0] >> arr[i][1];
+        }
+
+        int count;
+
+        cin >> count;
+
+        cout << "Case #" << t << ": ";
+        while (count--) {
+            int time;
+
+            cin >> time;
+
+            int answer = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i][0] <= time && time <= arr[i][1]) {
+                    answer++;
+                }
+            }
+
+            cout << answer << " ";
+        }
+        cout << "\n";
+    }
+}
+int main() {
+    fastio();
+    solve();
+}
