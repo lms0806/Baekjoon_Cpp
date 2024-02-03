d02def7006343713c28abbc08746607400a3c0d5
lms0806 <godmlzkf1@naver.com>
2024-02-03 22:43:30 +0900

    Create 18964.cpp

---
+++ 15001-20000/18964.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void fastio() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void solve() {
+    cout << "1 0";
+}
+int main() {
+    fastio();
+    solve();
+}
